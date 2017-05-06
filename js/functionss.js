8be0231715d62d872549a135d962bc9a5dbe2573
Simon2233 <simon.fu.1997@gmail.com>
2017-05-06 14:36:58 -0400

    Dynamic Input Boxes

--- js/functionss.js
+++ js/functionss.js
@@ -1,1 +1,9 @@
-test
+$(document).ready(function() {
+	var counter = 0;
+	$("#add").click(function() {
+		if (counter < 10) {
+			$("#input-container").append("<div id='left-input-container'><input type='number'></div><div id='right-input-container'><input type='number'></div>");
+			counter++;
+		}
+	});
+});
