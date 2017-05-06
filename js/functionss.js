a1a1c4e7f9f79a5bc244a27b6b5a354f65789cbc
ken-huang97 <ktkhuang@hotmail.com>
2017-05-06 15:14:39 -0400

    Rows, new style

--- js/functionss.js
+++ js/functionss.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 	var counter = 0;
 	$("#add").click(function() {
-		if (counter < 10) {
-			$("#input-container").append("<div id='left-input-container'><input type='number'></div><div id='right-input-container'><input type='number'></div>");
+		if (counter < 9) {
+			$("#row-container").append("<input class='left-input' type='number'><input class='right-input' type='number'>");
 			counter++;
 		}
 	});
