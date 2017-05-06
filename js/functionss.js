298216a6235c4fbb4eb20c259cb2649fb1f4ba28
Simon2233 <simon.fu.1997@gmail.com>
2017-05-06 16:31:26 -0400

    Handle Empty Inputs

--- js/functionss.js
+++ js/functionss.js
@@ -1,5 +1,11 @@
+// function saveData() {
+	
+// 	return true;
+// }
+
 $(document).ready(function() {
 	var counter = 0;
+	var numberOfRows = 1;
 	$("#add").click(function() {
 		if (counter < 9) {
 			$(".row-container").append("<div class='mark-row'><input class='left-input' type='number'><input class='right-input' type='number'></div>");
@@ -15,11 +21,15 @@ $(document).ready(function() {
 		$(".row-container div").each(function() {
 			var gradeValue;
 			var weightValue;
-			gradeValue = parseFloat(($(this).find("input.left-input").val()));
-			weightValue = parseFloat(($(this).find("input.right-input").val()));
-			totalWeight += weightValue;
-			grades.push(gradeValue);
-			weight.push(weightValue);
+			gradeValue = ($(this).find("input.left-input").val());
+			weightValue = ($(this).find("input.right-input").val());
+			if (gradeValue.length !== 0 && weightValue.length !== 0) {
+				gradeValue = parseFloat(gradeValue);
+				weightValue = parseFloat(weightValue) / 100;
+				totalWeight += weightValue;
+				grades.push(gradeValue);
+				weight.push(weightValue);
+			}
 		});
 		for (i = 0; i < grades.length; i++) {
 			totalGrades += grades[i]*weight[i];
