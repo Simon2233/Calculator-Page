4eea2640369075872e0fc6fa41fac3b312e247c9
Simon2233 <simon.fu.1997@gmail.com>
2017-05-06 15:17:33 -0400

    rewrite

--- js/functionss.js
+++ js/functionss.js
@@ -2,8 +2,17 @@ $(document).ready(function() {
 	var counter = 0;
 	$("#add").click(function() {
 		if (counter < 10) {
-			$("#input-container").append("<div id='left-input-container'><input type='number'></div><div id='right-input-container'><input type='number'></div>");
+			$("#input-container").append("<div class='left-input-container'><input type='number' class='grades'></div><div class='right-input-container'><input type='number' class='weight'></div>");
 			counter++;
 		}
+	});
+	$("#calculate").click(function() {
+		var grades = [];
+		var weight = [];
+		$("div.left-input-container").each(function() {
+			var inputValue;
+			inputValue = ($(this).find("input.grades").val());
+			console.log(inputValue);
+		});
 	});
 });
