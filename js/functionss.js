a32cdfe146b1e0563245fb511df40548a9101c3f
Simon2233 <simon.fu.1997@gmail.com>
2017-05-06 15:56:40 -0400

    Calculate Average

--- js/functionss.js
+++ js/functionss.js
@@ -2,17 +2,29 @@ $(document).ready(function() {
 	var counter = 0;
 	$("#add").click(function() {
 		if (counter < 9) {
-			$("#row-container").append("<div class='mark-row'><input class='left-input' type='number'><input class='right-input' type='number'><\div>");
+			$(".row-container").append("<div class='mark-row'><input class='left-input' type='number'><input class='right-input' type='number'></div>");
 			counter++;
 		}
 	});
 	$("#calculate").click(function() {
 		var grades = [];
 		var weight = [];
-		$("div.left-input-container").each(function() {
-			var inputValue;
-			inputValue = ($(this).find("input.grades").val());
-			console.log(inputValue);
+		var totalWeight = 0;
+		var totalGrades = 0;
+		var average;
+		$(".row-container div").each(function() {
+			var gradeValue;
+			var weightValue;
+			gradeValue = parseFloat(($(this).find("input.left-input").val()));
+			weightValue = parseFloat(($(this).find("input.right-input").val()));
+			totalWeight += weightValue;
+			grades.push(gradeValue);
+			weight.push(weightValue);
 		});
+		for (i = 0; i < grades.length; i++) {
+			totalGrades += grades[i]*weight[i];
+		}
+		average = totalGrades / totalWeight;
+		$("#result").text("Your Average is: " + average);
 	});
 });
