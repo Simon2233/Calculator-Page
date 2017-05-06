6629b1bffe0b325c3991197c7b96da5359b2e21e
ken-huang97 <ktkhuang@hotmail.com>
2017-05-06 17:48:00 -0400

    New row if tab on last box

---
+++ js/functionsk.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+	var counter = 0;
+	$("#row-container").on('blur','.mark-row:last-child input.right-input',function() {
+		setTimeout(function () {
+			//alert(document.activeElement.tagName);
+			if (document.activeElement.tagName == "BUTTON" && counter < 9) {
+				$("#row-container").append("<div class='mark-row'><input class='left-input' type='number'><input class='right-input' type='number'><\div>");
+				$(".mark-row:last-child input.left-input").focus();
+				counter++;
+			}
+		}, 0);
+	});
+});
